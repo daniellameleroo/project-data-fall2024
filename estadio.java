544f81dd17dacc431aa65dcf23e0085671cc8afc
daniellameleroo <daniella.melero@upr.edu>
2024-11-14 15:59:00 -0400

    estadio class solamente seats avaiables

---
+++ estadio.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+
+public class estadio {
+    private Set<asiento> avaiableSeats;
+    private HashMap<cliente, List<asiento>> reservations;
+    private LinkedList<String> transactionHistory;
+    private Stack<String> undoStack;
+    private Queue<cliente> waitingList;
+
+
+    public estadio(){
+        avaiableSeats = new HashSet<>();
+        reservations = new HashMap<>();
+        transactionHistory = new LinkedList<>();
+        undoStack = new Stack<>();
+        waitingList = new LinkedList<>();
+        initializeSeats();
+    }
+    private void initializeSeats(){
+        for (int i = 1; i <=500; i++){
+            avaiableSeats.add(new asiento("Field Level", i / 10 + 1, i));
+
+        }
+        for(int i = 1; i <= 1000; i++){
+            avaiableSeats.add(new asiento( "Main Level", i/ 20 + 1, i));
+
+        }
+        for(int i = 1; i <=2000; i++){
+            avaiableSeats.add(new asiento("Grandstand Level", i / 30 + 1, i));
+        }
+    }
+    
+
+    
+}
