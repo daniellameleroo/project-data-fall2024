65f4875989df268c547cbd6cc9f56a75abfc89da
daniellameleroo <daniella.melero@upr.edu>
2024-11-24 16:32:20 -0400

    Comments y updates asientos

--- src/Asiento.java
+++ src/Asiento.java
@@ -1,33 +1,83 @@
 package src;
 
-public class Asiento{
-    private String section;
-    private int row;
-    private int seatNumber;
+/**
+ * Se crea la Clase Asiento para representar un asiento de la Clase Estadio con
+ * su sección, fila y su número de asiento
+ */
+public class Asiento {
+    private String section; // Representa las secciones: Field Level, Main Level y Grandstand Level 
+    private int row; //Representa las filas 
+    private int seatNumber; //Representa el número de asiento
 
+    /**
+     * Creamos un constructor para crear el asiento 
+     * @param section La sección del asiento
+     * @param row La fila deel asiento
+     * @param seatNumber El número del asiento
+     */
 
-public Asiento(String section, int row, int seatNumber ){
-    this.section = section;
-    this.row = row;
-    this.seatNumber = seatNumber;
-}
+    public Asiento(String section, int row, int seatNumber) {
+        this.section = section;
+        this.row = row;
+        this.seatNumber = seatNumber;
+    }
+/** 
+ * Este string me permite coger la sección 
+ * 
+ * @return el nombre de la sección
+ */
+    public String getSection() {
+        return section;
 
+    }
+/**
+ * Este integer me permite devolver el número de la fila 
+ * @return número de fila
+ */
+    public int getRow() {
+        return row;
 
-public String getSection(){
-    return section;
+    }
+/**
+ * Este integer me permite devolver el número del asiento
+ * @return número de asiento
+ */
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+/**
+ * Al iagual que en Cliente hicimos un string que nos devuelva la representación del Asiento 
+ */
+    @Override
+    public String toString() {
+        return "Section: " + section + ", Row: " + row + ", Seat: " + seatNumber;
+    }
+/**
+ * Hicimos esta parte para verificar si hay dos asientos basado en la sección, fila y número de asiento
+ * @param obj Lo usamos para comparar
+ * @return True si hay dos asientos iguales y False que no hay duplicados
+ */
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
 
-}
-public int getRow(){
-    return row;
+        Asiento asiento = (Asiento) obj;
 
-}
-
-public int getSeatNumber(){
-    return seatNumber;
-}
-
-@Override
-public String toString(){
-    return "Section: " + section + ", Row: " + row + ", Seat: " + seatNumber;
+        return row==asiento.row &&
+        seatNumber == asiento.seatNumber &&
+        section.equals(asiento.section);
+    }
+/**
+ * Hacemos un Hash Code para el cliente basado en su section, fila y número de asiento asi salva su información. Hash Code me deja identificar los objetos rapidamente y optimiza el trabajo cuando se guarda datos o cuando se sacan datos.
+ * Si salen dos equals como en el booleano de arriba, se ponen en el mismo código de hash. 
+ * Escogimos que se multiplique con 31 por que es un número primario Odd que permite la distribucción mas organizada y minimiza las coliciones. Es una practica común y pensamos que distribuía mejor. Ayuda a combinar la sección, fila y # de asiento en un valor númerico. 
+ */
+    @Override
+    public int hashCode(){
+        int result = section.hashCode();
+        result = 31* result + row;
+        result = 31 * result + seatNumber;
+        return result;
     }
 }
