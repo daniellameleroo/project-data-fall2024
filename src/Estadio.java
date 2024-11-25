25502ff93f4caedd4282a2c44c13a0e606b26c2a
daniellameleroo <daniella.melero@upr.edu>
2024-11-24 22:58:32 -0400

    price estadio

--- src/Estadio.java
+++ src/Estadio.java
@@ -1,20 +1,13 @@
 package src;
 import java.util.*;
 
-<<<<<<< HEAD:Estadio.java
-public class Estadio {
-    private Set<Asiento> avaiableSeats; //Se hace un set para los asientos disponibles
-    private HashMap<Cliente, List<Asiento>> reservations;
-=======
-
-
 public class Estadio {
     private Set<Asiento> avaiableSeats;
     private HashMap<Cliente, List<Asiento>> reservations;
->>>>>>> 8097d41 (new file main):src/Estadio.java
     private LinkedList<String> transactionHistory;
     private Stack<String> undoStack;
     private Queue<Cliente> waitingList;
+    
 
 
     public Estadio(){
@@ -31,28 +24,32 @@ public class Estadio {
     }
     private void initializeSeats(){
         for (int i = 1; i <=500; i++){
-            avaiableSeats.add(new Asiento("Field Level", i / 10 + 1, i));
+            avaiableSeats.add(new Asiento("Field Level", i / 10 + 1, i, 300.0));
 
         }
         for(int i = 1; i <= 1000; i++){
-            avaiableSeats.add(new Asiento( "Main Level", i/ 20 + 1, i));
+            avaiableSeats.add(new Asiento( "Main Level", i/ 20 + 1, i, 120.0));
 
         }
         for(int i = 1; i <=2000; i++){
-            avaiableSeats.add(new Asiento("Grandstand Level", i / 30 + 1, i));
+            avaiableSeats.add(new Asiento("Grandstand Level", i / 30 + 1, i, 45.0));
         }
+        
     }
     
     private void initializeReservations(){
-      
+      reservations.clear();
     }
     private void initializeTransactionHistory(){
-
+        transactionHistory.clear();
     }
     private void initializeUndoStack(){
-
+        undoStack.clear();
     }
     private void initializeWaitingList(){
+        waitingList.clear();
 
     }
+  
+    
 }
